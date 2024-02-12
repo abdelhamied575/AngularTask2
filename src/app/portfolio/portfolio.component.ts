52f200d1620c4395dc584d331668cf0d9b93d7c3
AbdelhamedBelal <abdelhamiedb@gmail.com>
2024-02-12 18:54:24 +0200

    upload

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,23 @@
+import { Component,OnInit } from '@angular/core';
+
+//declare var $: any;
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+export class PortfolioComponent implements OnInit{
+
+  ngOnInit(): void {
+    
+    // let x=document.getElementById("one");
+    // // x?.addEventListener("click",function(){
+    // //   console.log("Hiiiiiiiiiiiiiii")
+    // // })
+    // console.log(x)
+    
+    
+  }
+
+}
